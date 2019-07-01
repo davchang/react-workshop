e3d87e57dffcbc90d3e53796adff5e2fee9e8cdf
David Chang <dchang@strongtie.com>
2019-06-30 17:07:45 -0700

    add 22-use-redux

---
+++ subjects/22-use-redux/index-h-deprecate.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function HelloWorld() {
+  return (
+    <div className='helloWorld'>
+      <div className='helvetica-9'>Hello World! This should be heavy as it's done in Helvetica-9 Heavy!</div>
+
+      <p>test</p>
+
+      <img src='src/globals/images/technical-difficulties.svg' />
+    </div>
+  )
+}
