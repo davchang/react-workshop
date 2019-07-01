e3e4daf7ea63ed79edb5ea951042fce46e239185
David Chang <dchang@strongtie.com>
2019-07-01 16:00:22 -0700

    use useRef

--- subjects/05-Imperative-to-Declarative/solution.js
+++ subjects/05-Imperative-to-Declarative/solution.js
@@ -27,39 +27,65 @@ function Modal({ children, title, isOpen }) {
   );
 
   return (
-    <div className="modal fade" ref={modalRef}>
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h4 className="modal-title">{title}</h4>
+      <div className="modal fade" ref={modalRef}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">{title}</h4>
+            </div>
+            <div className="modal-body">{children}</div>
           </div>
-          <div className="modal-body">{children}</div>
         </div>
       </div>
-    </div>
   );
 }
 
-function App() {
-  const [isModalOpen, setModalOpen] = useState(false);
+function useModal(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  const myRef = useRef(null);
 
   function openModal() {
-    setModalOpen(true);
+    setValue(true);
   }
 
   function closeModal() {
-    setModalOpen(false);
+    setValue(false);
   }
+
+  return ({
+    ref: myRef,
+    value: value,
+    openModal: openModal,
+    closeModal: closeModal
+  })
+}
+
+function App() {
+  const uploadModal = useModal(false)
+  const settingsModal = useModal(false)
+  // const [isModalOpen, setModalOpen] = useState(false);
+  //
+  // function openModal() {
+  //   setModalOpen(true);
+  // }
+  //
+  // function closeModal() {
+  //   setModalOpen(false);
+  // }
 
   return (
     <div className="container">
       <h1>Let’s make bootstrap modal declarative</h1>
 
-      <button className="btn btn-primary" onClick={openModal}>
-        open modal
+      <button className="btn btn-primary" onClick={uploadModal.openModal}>
+        open upload modal
       </button>
 
-      <Modal title="Declarative is better" isOpen={isModalOpen}>
+      <button className="btn btn-primary" onClick={settingsModal.openModal}>
+        open settings modal
+      </button>
+
+      <Modal title="Declarative is better" isOpen={uploadModal.value}>
         <p>Calling methods on instances is a FLOW not a STOCK!</p>
         <p>
           It’s the dynamic process, not the static program in text
@@ -70,7 +96,26 @@ function App() {
           of state.
         </p>
         <button
-          onClick={closeModal}
+          onClick={uploadModal.closeModal}
+          type="button"
+          className="btn btn-default"
+        >
+          Close
+        </button>
+      </Modal>
+
+      <Modal title="Declarative is better" isOpen={settingsModal.value}>
+        <p>Calling methods on instances is a FLOW not a STOCK!</p>
+        <p>
+          It’s the dynamic process, not the static program in text
+          space.
+        </p>
+        <p>
+          You have to experience it over time, rather than in snapshots
+          of state.
+        </p>
+        <button
+          onClick={settingsModal.closeModal}
           type="button"
           className="btn btn-default"
         >
